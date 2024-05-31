7edab561400dd7154de823a9f86003bad466f7a2
Ivan Shukhov <45999338+ivanshukhov@users.noreply.github.com>
2024-05-31 12:20:06 +0200

    adjust pool hash

--- contracts/libraries/PoolAddress.sol
+++ contracts/libraries/PoolAddress.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.5.0;
 
 /// @title Provides functions for deriving a pool address from the factory, tokens, and the fee
 library PoolAddress {
-    bytes32 internal constant POOL_INIT_CODE_HASH = 0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54;
+    bytes32 internal constant POOL_INIT_CODE_HASH = 0xc2da9e6e524eb944c4b2cc7b6c49696cc4b64e53467d21befda7a3c1d79a3395;
 
     /// @notice The identifying key of the pool
     struct PoolKey {
